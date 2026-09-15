8c3ae1446dc9938c333c2af837a0ed8282df141f
Gage Mckenzie <gagemckenzie70@gmail.com>
2026-09-15 11:22:21 -0300

    changing position of object

--- Assets/objectBehavior.cs
+++ Assets/objectBehavior.cs
@@ -2,14 +2,25 @@ using UnityEngine;
 
 public class objectBehavior : MonoBehaviour
 {
+    public Vector2 test;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        test = transform.position;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        transform.position = new Vector3 (2.9f, 9.39f,0f);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
     {
         
     }
